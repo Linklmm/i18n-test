efef8513b7dbc771af32b6c4da6a88c0c2cd953e
minmin.liu <minmin.liu@linkedcare.cn>
2021-12-29 17:05:04 +0800

    Initial commit

---
+++ src/test/java/com/lmm/i18ntest/I18nTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmm.i18ntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
